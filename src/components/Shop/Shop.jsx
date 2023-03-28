01424ff584e334d64e45b74fbc57079c1584786c
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-03-29 00:02:42 +0600

    retrieve product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,10 +14,33 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = []
+        // step 1: get id
+        for (const id in storedCart) {
+            // step 2: get the product by using id 
+            const addedProduct = products.find(product => product.id == id)
+            if (addedProduct) {
+                // step 3: get quantity of the product
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                // step 4: add the addedProduct to the saved cart
+                savedCart.push(addedProduct)
+            }
+            // console.log(addedProduct);
+
+        }
+        //  setp 5: set the cart
+        setCart(savedCart)
+    }, [products])
+
     const handleAddToCart = (product) => {
         // cart.push(product); 
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
@@ -32,7 +56,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <Cart
-                cart = {cart}
+                    cart={cart}
                 ></Cart>
             </div>
         </div>
